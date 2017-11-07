a171992007cd34e0ea91193e13429494fa0517dd
Omar Farooqi <k1631089@kcl.ac.uk>
2017-11-07 10:31:22 +0000

    Some code

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
+<?php 
+
+$test = "Lets do it in Node.JS";
+
+?>
+
 <html>
 <head>
 	<title>Hello World</title>
 </head>
 <body>
 <h1>Hello World</h1>
+<h2><?php echo $test; ?></h2>
 </body>
 </html>
