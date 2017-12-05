168aa50ccdc57ad4d09c56982cbe0d7c60a99abe
Dimitris Papatheodoulou <dimitrispapatheodoulou@gmail.com>
2017-12-05 20:59:03 +0000

    Add logout button & change navigation bar layout

--- config.php
+++ config.php
@@ -7,17 +7,17 @@
 // define('SB_DB_NAME', 'lab-indigo'); // db name
 
 // Cloud 9
-// define('SB_DB_SERVER', getenv('IP')); // db server
-// define('SB_DB_USER', getenv('C9_USER')); // db user
-// define('SB_DB_PASS', '');
-// define('SB_DB_PORT', 3306); // db password
-// define('SB_DB_NAME', 'c9'); // db name
+define('SB_DB_SERVER', getenv('IP')); // db server
+define('SB_DB_USER', getenv('C9_USER')); // db user
+define('SB_DB_PASS', '');
+define('SB_DB_PORT', 3306); // db password
+define('SB_DB_NAME', 'c9'); // db name
 
 // Heroku configs
-define('SB_DB_SERVER', 'eu-cdbr-west-01.cleardb.com'); // db server
-define('SB_DB_USER', 'b3fc62381bbceb'); // db user
-define('SB_DB_PASS', '5b958dbe'); // db password
-define('SB_DB_NAME', 'heroku_173da242a6954ae'); // db name
+// define('SB_DB_SERVER', 'eu-cdbr-west-01.cleardb.com'); // db server
+// define('SB_DB_USER', 'b3fc62381bbceb'); // db user
+// define('SB_DB_PASS', '5b958dbe'); // db password
+// define('SB_DB_NAME', 'heroku_173da242a6954ae'); // db name
 
 $conn = mysqli_connect(SB_DB_SERVER, SB_DB_USER, SB_DB_PASS, SB_DB_NAME);
 
