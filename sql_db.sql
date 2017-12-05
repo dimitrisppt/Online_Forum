6c5b720c9d186a790eb2fbd92fe183e71a954139
Omar Farooqi <k1631089@kcl.ac.uk>
2017-12-05 10:17:36 +0000

    Fixed questions reply issue, metadata & login issue

--- sql_db.sql
+++ sql_db.sql
@@ -1,22 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
+-- version 4.4.10
+-- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Dec 04, 2017 at 01:06 PM
--- Server version: 5.6.34-log
--- PHP Version: 7.1.5
+-- Generation Time: Dec 05, 2017 at 10:50 AM
+-- Server version: 5.5.42
+-- PHP Version: 7.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `lab-indigo`
@@ -31,30 +23,32 @@ SET time_zone = "+00:00";
 CREATE TABLE `lab_posts` (
   `post_id` int(11) NOT NULL,
   `subject` text NOT NULL,
-  `message` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `message` text NOT NULL,
+  `username` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lab_posts`
 --
 
-INSERT INTO `lab_posts` (`post_id`, `subject`, `message`) VALUES
-(1, 'Does this work?', 'Yes it works'),
-(2, 'New Question', 'New Post'),
-(3, 'test subj', 'test msg'),
-(4, 'Hello', 'Hello fkuhufish'),
-(5, 'Hello', 'Hello fkuhufish'),
-(6, 'fgfg', 'dfgdfg'),
-(7, 'fgfg', 'dfgdfg'),
-(8, 'dfhuisdh', 'lfihohdfodwfhf'),
-(9, 'This is 2132', 'qhiuhiuhiwhIDHWFIF'),
-(10, 'This is a question', 'this is an answer'),
-(11, 'This is a question', 'this is an answer'),
-(12, 'hfiufhd', 'sfdhufdhs'),
-(13, 'This is a question #2', 'This is a message #2'),
-(14, 'Test', 'testno3\r\n'),
-(15, 'Adding a question', 'question message'),
-(16, 'This is a test of height', 'Test of height\r\nTest of height\r\nTest of height\r\nTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of \r\nTest of height\r\nTest of height\r\nTest of height\r\nTest of height\r\nTest of height');
+INSERT INTO `lab_posts` (`post_id`, `subject`, `message`, `username`) VALUES
+(1, 'Does this work?', 'Yes it works', NULL),
+(2, 'New Question', 'New Post', NULL),
+(3, 'test subj', 'test msg', NULL),
+(4, 'Hello', 'Hello fkuhufish', NULL),
+(5, 'Hello', 'Hello fkuhufish', NULL),
+(6, 'fgfg', 'dfgdfg', NULL),
+(7, 'fgfg', 'dfgdfg', NULL),
+(8, 'dfhuisdh', 'lfihohdfodwfhf', NULL),
+(9, 'This is 2132', 'qhiuhiuhiwhIDHWFIF', NULL),
+(10, 'This is a question', 'this is an answer', NULL),
+(11, 'This is a question', 'this is an answer', NULL),
+(12, 'hfiufhd', 'sfdhufdhs', NULL),
+(13, 'This is a question #2', 'This is a message #2', NULL),
+(14, 'Test', 'testno3\r\n', NULL),
+(15, 'Adding a question', 'question message', NULL),
+(16, 'This is a test of height', 'Test of height\r\nTest of height\r\nTest of height\r\nTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of heightTest of \r\nTest of height\r\nTest of height\r\nTest of height\r\nTest of height\r\nTest of height', NULL),
+(17, 'Username Post', 'works good', 'user1');
 
 -- --------------------------------------------------------
 
@@ -66,7 +60,7 @@ CREATE TABLE `login` (
   `id` int(11) NOT NULL,
   `username` varchar(15) NOT NULL,
   `password` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `login`
@@ -85,24 +79,18 @@ INSERT INTO `login` (`id`, `username`, `password`) VALUES
 CREATE TABLE `post_replies` (
   `reply_id` int(11) NOT NULL,
   `subject` varchar(30) NOT NULL,
-  `message` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `message` text NOT NULL,
+  `post_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `post_replies`
 --
 
-INSERT INTO `post_replies` (`reply_id`, `subject`, `message`) VALUES
-(1, 'Check reply ', 'check reply message'),
-(2, 'check reply #2', 'check #2'),
-(3, 'Thats the answer', 'answer'),
-(4, 'test', 'test'),
-(5, 'Message', 'new message'),
-(6, 'blank page test', 'blank page'),
-(7, 'new reply', 'reply message'),
-(8, 'odiufsdiu', 'dsfkhdsfiuhsfd'),
-(9, 'kuhfiuhafi', 'kjhisdhffsdhfds'),
-(10, 'ddsffd', 'sdffs');
+INSERT INTO `post_replies` (`reply_id`, `subject`, `message`, `post_id`) VALUES
+(16, 'test4', 'test4', 1),
+(17, 'love it ', 'thanks bro', 2),
+(18, 'works good', 'yep', 3);
 
 -- --------------------------------------------------------
 
@@ -115,7 +103,7 @@ CREATE TABLE `sign_up` (
   `email` varchar(50) NOT NULL,
   `username` varchar(15) NOT NULL,
   `password` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `sign_up`
@@ -169,23 +157,19 @@ ALTER TABLE `sign_up`
 -- AUTO_INCREMENT for table `lab_posts`
 --
 ALTER TABLE `lab_posts`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `post_replies`
 --
 ALTER TABLE `post_replies`
-  MODIFY `reply_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `reply_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
 --
 -- AUTO_INCREMENT for table `sign_up`
 --
 ALTER TABLE `sign_up`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
