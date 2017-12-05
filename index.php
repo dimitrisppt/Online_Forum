adeb760239312317dd21a90dc100c156862ad2f4
pragyasinha265 <k1631144@nmscde004147.nms.kcl.ac.uk>
2017-12-05 09:27:51 +0000

    Changes to the main layout

--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 
 include('config.php');
 
-if ($_POST["subject"] && $_POST["message"]) {
-    $q = "INSERT INTO lab_posts (subject, message) VALUES ('" . $_POST["subject"] . "','" . $_POST["message"] . "')";
-    $conn->query($q);
-}
+// if($_SESSION['username']) {
+    if ($_POST["subject"] && $_POST["message"]) {
+        $q = "INSERT INTO lab_posts (subject, message) VALUES ('" . $_POST["subject"] . "','" . $_POST["message"] . "')";
+        $conn->query($q);
+    }
 
-$q = "SELECT * FROM lab_posts";
-$result = $conn->query($q);
+    $q = "SELECT * FROM lab_posts";
+    $result = $conn->query($q);
+// }
 
 
 ?>
@@ -23,7 +25,11 @@ $result = $conn->query($q);
     <meta http-equiv="Content-type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 
 <body>
@@ -32,14 +38,41 @@ $result = $conn->query($q);
             <div id="Header">
                 <h1 id="HeaderTitle"><span>Forum</span></h1>
                 <div id="navigation">
-                    <ul>
-                      <li><a href="<?php echo 'login.php'; ?>">Login</a></li>
-                      <li><a href="<?php echo 'signup.php'; ?>">Sign Up</a></li>
-                      <li><a href="<?php echo 'addPost.php'; ?>">Add Post</a></li>
-                    </ul>
+
+                      <a href="<?php echo 'login.php'; ?>">Login</a>
+                      <a href="<?php echo 'signup.php'; ?>">Sign Up</a>
+                      <a href="<?php echo 'addPost.php'; ?>">Add Post</a>
+                      <div class="search">
+                        <form action="./">
+                          <input type="text" placeholder="Search for a topic..." name="search">
+                          <button type="submit">Search</i></button>
+                        </form>
+                      </div>
+
                 </div>
             </div>
         </div>
+
+        <div id="Content">
+            <div class="sectionTitle">
+                <p>Questions: </p>
+            </div>
+            <div id="questionList" class="questionList">
+
+                    <?php
+                        while($row = $result->fetch_assoc()) {
+                            echo '<div id="questionSection" class="questionSection">';
+                                echo '<a href="./question.php?id=' . $row["post_id"] . '">';
+                                // echo $row["post_id"];
+                                echo "<h2>" . $row["subject"] . "</h2></a>";
+                            echo '</div>';
+                        }
+                    ?>
+
+            </div>
+
+
+		</div>
 
         <?php
 
@@ -51,7 +84,7 @@ $result = $conn->query($q);
         }
 
         ?>
-
+        <footer>Copyright &copy; Team Indigo (SEG Lab Project)</footer>
 
     </div>
 </body>
--- signup.php
+++ signup.php
@@ -2,8 +2,8 @@
 
 include('config.php');
 
-if ($_POST["fullname"] && $_POST["username"] && $_POST["password"]) {
-    $su = "INSERT INTO sign_up (fullname, username, password) VALUES ('" . $_POST["fullname"] . "','" . $_POST["username"] . "','" . $_POST["password"] . "')";
+if ($_POST["email"] && $_POST["username"] && $_POST["password"]) {
+    $su = "INSERT INTO sign_up (email, username, password) VALUES ('" . $_POST["email"] . "','" . $_POST["username"] . "','" . $_POST["password"] . "')";
     $conn->query($su);
     header("Location: ./");
     die();
@@ -23,35 +23,76 @@ $result = $conn->query($su);
 	    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	    <link rel="stylesheet" type="text/css" href="css/main.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	    <link rel="stylesheet" type="text/css" href="main.css">
 
 		<body>
 		    <div id="MainContainer">
-		        <div id="HeaderContainer">
-		            <div id="Header">
-		                <h1 id="HeaderTitle"><span>Forum</span></h1>
-						<div id="navigation">
-							<ul>
-		                      <li><a href="<?php echo 'login.php'; ?>">Login</a></li>
-                              <li><a href="<?php echo 'signup.php'; ?>">Sign Up</a></li>
-		                      <li><a href="<?php echo 'index.php'; ?>">Home</a></li>
-		                    </ul>
-						</div>
-					</div>
-				</div>
+                    <div id="HeaderContainer">
+                        <div id="Header">
+                            <h1 id="HeaderTitle"><span>Forum</span></h1>
+                            <div id="navigation">
 
+                                  <a href="<?php echo 'login.php'; ?>">Login</a>
+                                  <a href="<?php echo 'signup.php'; ?>">Sign Up</a>
+                                  <a href="<?php echo 'index.php'; ?>">Home</a>
+                                  <div class="search">
+                                    <form action="./">
+                                      <input type="text" placeholder="Search for a topic..." name="search">
+                                      <button type="submit">Search</i></button>
+                                    </form>
+                                  </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+            <div id="Content">
                 <div id="Login">
-                    <div id="LoginForm">
+                    <!-- <div id="LoginForm">
                         <form action="./signup.php" method="post">
                             <h2 id="Details"</h2><span style="color: black">Please enter your details: </span><br><br>
-                                Full Name: <input type="text" class="form" name="fullname" id="fullname" /><br>
+                                Email: <input type="text" class="form" name="email" id="email" /><br>
                                 Username: <input type="text" class="form" name="username" id="username" /><br>
                                 Password: <input type="password" class="form" name="password" id="password" /><br>
 
                             <input type="submit" class="form" value="Sign Up" />
                         </form>
+                    </div> -->
+
+                        <div class="container">
+                          <h3>Sign Up</h3>
+                          <style>
+                            label {float: left}
+                            button {float: right}
+                          </style>
+                          <form action="./signup.php" method="post">
+                            <div class="form-group">
+                              <label for="email">Email:</label>
+                              <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                            </div>
+                            <div class="form-group">
+                              <label for="username">Username:</label>
+                              <input type="text" class="form-control" id="username" placeholder="Enter username" name="username">
+                            </div>
+                            <div class="form-group">
+                              <label for="password">Password:</label>
+                              <input type="password" class="form-control" id="password" placeholder="Enter password" name="password">
+                            </div>
+                            <div class="checkbox">
+                              <label><input type="checkbox" name="remember"> Remember me</label>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Sign up</button>
+                          </form>
+                        </div>
+
                     </div>
                 </div>
+                <footer>Copyright &copy; Team Indigo (SEG Lab Project)</footer>
+            </div>
         </body>
 
     </head>
