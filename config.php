9746b65cf58dcdc17fa38ba47877a59b3a394464
Omar Farooqi <k1631089@kcl.ac.uk>
2017-12-05 10:40:37 +0000

    Repush heroku

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
 // Local configs
-define('SB_DB_SERVER', 'localhost'); // db server
-define('SB_DB_USER', 'root'); // db user
-define('SB_DB_PASS', 'root'); // db password
-define('SB_DB_NAME', 'lab-indigo'); // db name
+// define('SB_DB_SERVER', 'localhost'); // db server
+// define('SB_DB_USER', 'root'); // db user
+// define('SB_DB_PASS', 'root'); // db password
+// define('SB_DB_NAME', 'lab-indigo'); // db name
 
 // Cloud 9
 // define('SB_DB_SERVER', getenv('IP')); // db server
@@ -14,10 +14,10 @@ define('SB_DB_NAME', 'lab-indigo'); // db name
 // define('SB_DB_NAME', 'c9'); // db name
 
 // Heroku configs
-// define('SB_DB_SERVER', 'eu-cdbr-west-01.cleardb.com'); // db server
-// define('SB_DB_USER', 'b3fc62381bbceb'); // db user
-// define('SB_DB_PASS', '5b958dbe'); // db password
-// define('SB_DB_NAME', 'heroku_173da242a6954ae'); // db name
+define('SB_DB_SERVER', 'eu-cdbr-west-01.cleardb.com'); // db server
+define('SB_DB_USER', 'b3fc62381bbceb'); // db user
+define('SB_DB_PASS', '5b958dbe'); // db password
+define('SB_DB_NAME', 'heroku_173da242a6954ae'); // db name
 
 $conn = mysqli_connect(SB_DB_SERVER, SB_DB_USER, SB_DB_PASS, SB_DB_NAME);
 
