8443d550842c12f72d36ce0e75ba644d4088cc50
Pragya Sinha <pragyasinha265@hotmail.com>
2017-12-05 09:52:13 +0000

    added header and footer

--- question.php
+++ question.php
@@ -1,7 +1,7 @@
 <?php
 
 include('config.php');
-
+include('header.php');
 $result = null;
 if ($_GET["id"]) {
 	$q = "SELECT * FROM lab_posts WHERE post_id=" . $_GET["id"];
@@ -20,45 +20,6 @@ $su = "SELECT * FROM post_replies";
 $replyResult = $conn->query($su);
 
 ?>
-
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Forum</title>
-
-    <meta charset="utf-8">
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-    <link rel="stylesheet" type="text/css" href="main.css">
-</head>
-
-<body>
-	<div id="MainContainer">
-        <div id="HeaderContainer">
-            <div id="Header">
-                <h1 id="HeaderTitle"><span>Forum</span></h1>
-                <div id="navigation">
-
-                      <a href="<?php echo 'login.php'; ?>">Login</a>
-                      <a href="<?php echo 'signup.php'; ?>">Sign Up</a>
-                      <a href="<?php echo 'index.php'; ?>">Home</a>
-                      <div class="search">
-                        <form action="./">
-                          <input type="text" placeholder="Search for a topic..." name="search">
-                          <button type="submit">Search</i></button>
-                        </form>
-                      </div>
-
-                </div>
-            </div>
-        </div>
 
 
 		<div id="Content">
@@ -119,7 +80,9 @@ $replyResult = $conn->query($su);
 			</div>
 		</div>
 
-		<footer>Copyright &copy; Team Indigo (SEG Lab Project)</footer>
+		<?php
+	include ('footer.php');
+?>
     </div>
 
 </body>
