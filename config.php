60576c90c161f8edf3214baa68543cc40b5d7b38
Omar Farooqi <k1631089@kcl.ac.uk>
2017-12-05 09:48:20 +0000

    Fixed config

--- config.php
+++ config.php
@@ -6,10 +6,10 @@
 // define('SB_DB_NAME', 'lab-indigo'); // db name
 
 // mysql://b3fc62381bbceb:5b958dbe@eu-cdbr-west-01.cleardb.com/heroku_173da242a6954ae?reconnect=true
-define('SB_DB_SERVER', 'eu-cdbr-west-01.cleardb.com/heroku_173da242a6954ae?reconnect=true'); // db server
+define('SB_DB_SERVER', 'eu-cdbr-west-01.cleardb.com'); // db server
 define('SB_DB_USER', 'b3fc62381bbceb'); // db user
 define('SB_DB_PASS', '5b958dbe'); // db password
-define('SB_DB_NAME', 'indigo-lab'); // db name
+define('SB_DB_NAME', 'heroku_173da242a6954ae'); // db name
 
 $conn = mysqli_connect(SB_DB_SERVER, SB_DB_USER, SB_DB_PASS, SB_DB_NAME);
 
