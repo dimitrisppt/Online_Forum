28ee14446980812c739ca2846632f481ebcc45e1
Pragya Sinha <pragyasinha265@hotmail.com>
2017-12-05 09:51:45 +0000

    added header and footer

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 include('config.php');
+include('header.php');
 
 // if($_SESSION['username']) {
     if ($_POST["subject"] && $_POST["message"]) {
@@ -15,45 +16,7 @@ include('config.php');
 
 ?>
 
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Forum</title>
-
-    <meta charset="utf-8">
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-    <link rel="stylesheet" type="text/css" href="main.css">
-</head>
-
-<body>
-    <div id="MainContainer">
-        <div id="HeaderContainer">
-            <div id="Header">
-                <h1 id="HeaderTitle"><span>Forum</span></h1>
-                <div id="navigation">
-
-                      <a href="<?php echo 'login.php'; ?>">Login</a>
-                      <a href="<?php echo 'signup.php'; ?>">Sign Up</a>
-                      <a href="<?php echo 'addPost.php'; ?>">Add Post</a>
-                      <div class="search">
-                        <form action="./">
-                          <input type="text" placeholder="Search for a topic..." name="search">
-                          <button type="submit">Search</i></button>
-                        </form>
-                      </div>
-
-                </div>
-            </div>
-        </div>
-
-        <div id="Content">
+<div id="Content">
             <div class="sectionTitle">
                 <p>Questions: </p>
             </div>
@@ -84,7 +47,9 @@ include('config.php');
         }
 
         ?>
-        <footer>Copyright &copy; Team Indigo (SEG Lab Project)</footer>
+       <?php
+	include ('footer.php');
+?>
 
     </div>
 </body>
