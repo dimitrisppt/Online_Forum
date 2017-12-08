284af15e6d24fa2e7eb75da2bfe1aa96101fc07e
Dimitris Papatheodoulou <dimitrispapatheodoulou@gmail.com>
2017-12-08 21:01:54 +0000

    Improve UI & add additional features

--- index.php
+++ index.php
@@ -12,33 +12,68 @@ if ($_POST["subject"] && $_POST["message"]) {
 $q = "SELECT * FROM lab_posts";
 $result = $conn->query($q);
 
+$SessionUser = $_SESSION["username"];
+
+$nrq= $conn->query("SELECT COUNT(reply_id) FROM post_replies");
+$numberOfReplies = $nrq->fetch_assoc();
+
 ?>
 
 <div id="Content">
             <div class="sectionTitle">
                 <p>Questions: </p>
             </div>
+           
+            
+            
             <div id="questionList" class="questionList">
 
-
                 <?php
+                    
                     while($row = $result->fetch_assoc()) {
+                        
+                       
                         echo '<div id="questionSection" class="questionSection">';
-                            echo '<a href="./question.php?id=' . $row["post_id"] . '">';
-                            echo "<h2>" . $row["subject"] . "</h2></a>";
-                            echo '<span class="date_posted">' . $row["data_posted"] . '</span>';
-                            if ($row["username"]) {
-                                echo '<span class="username">' . $row["username"] . '</span>';
-                            } else {
-                                echo '<span class="username">Anonymous</span>';
-                            }
+                            echo  '<div id="c1">';
+                                echo '<h4 id="questionTitles">' . "Discussion" . "</h4>";
+                                echo '<a href="./question.php?id=' . $row["post_id"] . '">';
+                                echo "<h4>" . $row["subject"] . "</h4></a>";
+                            echo '</div>';
+                            
+                            echo  '<div id="c2">';
+                                echo '<h4 id="questionTitles">' . "Author" . "</h4>";
+                                echo '<img src="user.png" style="width:20px; height:20px;"/>';
+                                if ($row["username"]) {
+                                    echo '<span class="username">' . $row["username"] . '</span>';
+                                } else {
+                                    echo '<span class="username">Anonymous</span>';
+                                }
+                            echo '</div>'; 
+                            
+                            echo  '<div id="c3">';
+                                echo '<h4 id="questionTitles">' . "Replies" . "</h4>";
+                                echo '<span class="numberOfReplies">' . $numberOfReplies . '</span>';
+                            echo '</div>';
+                            
+                            echo  '<div id="c4">';
+                                echo '<h4 id="questionTitles">' . "Date Posted" . "</h4>";
+                                echo '<span class="date_posted">' . $row["data_posted"] . '</span>';
+                            echo '</div>';
+                                    
                         echo '</div>';
+                       
                     }
                 ?>
 
             </div>
-
-
+            <?php
+              
+            if ($_SESSION["username"]) {
+                                echo "<h5>" . "Logged in as: " . $SessionUser . "</5> <style> h5 {float:right; padding-right: 2%; font-style: italic} </style>";
+                            } else {
+                                echo "<h5>" . "You are not yet signed in." . "</h5> <style> h5 {float:right; padding-right: 2%; font-style: italic} </style>";
+                            }
+            ?>
 		</div>
         
        <?php
--- question.php
+++ question.php
@@ -21,7 +21,16 @@ if ($_GET["id"]) {
 $su = "SELECT * FROM post_replies WHERE post_id=" . $_GET["id"];
 $replyResult = $conn->query($su);
 
+$SessionUser = $_SESSION["username"];
+
+
+$row = $result->fetch_assoc();
+$subject = $row["subject"];
+
+
 ?>
+
+</script>
 
 		<div id="Content">
 			<h2 class="h2Titles">Question</h2>
@@ -39,25 +48,52 @@ $replyResult = $conn->query($su);
 						echo "</h3>";
 						echo '</div>';
 						echo '</div>';
+						echo "test";
 					}
+
+					
 				?>
 
 			<hr id="titleBar2">
+			
 			<h2 class="h2Titles">Answers</h2>
 			<div id="questionList" class="questionList">
 				<?php
 					while($row = $replyResult->fetch_assoc()) {
-						echo '<div id="questionSection" class="questionSection">';
-							// echo $row["post_id"];
-							echo "<p>" . $row["message"] . "</p>";
-							echo '<span class="date_posted">' . $row["date_posted"] . '</span>';
-                            if ($row["username"]) {
-                                echo '<span class="username">' . $row["username"] . '</span>';
-                            } else {
-                                echo '<span class="username">Anonymous</span>';
-                            }
-
-						echo '</div>';
+						
+						echo '<div id="answerSection" class="answerSection">';
+							echo '<div id="replyTitleAndMessage">';
+								echo '<div id="replySectionTitle" class="replySectionTitle">';
+								echo '<br>';
+								
+									echo  '<div id="c5">';
+										echo '<h4>'; 
+										echo '<img src="user.png" style="width:20px; height:20px;"/>';
+										echo " RE: " . $subject . "</h4>";
+										echo '<div id="subTitle">';
+			                                echo '<p>' . "by";
+			                                if ($row["username"]) {
+			                                    echo '<span class="user">' . " " . $row["username"] . '</span>';
+			                                } else {
+			                                    echo '<span class="user">' . " " . "Anonymous" . '</span>';
+			                                }
+			                                echo '<span class="date_posted">'. $row["data_posted"] . '</span>';
+			                                echo "</p>";
+			                             echo '</div>'; 
+		                            echo '</div>'; 
+		        				
+		        				echo '<br>';
+		                        echo '</div>';
+		                        
+		                        
+		                        echo '<div id="replySectionMessage" class="replySectionMessage">';
+		                        	echo "<p>" . $row["message"] . "</p>";
+		                        echo '</div>';
+		                        echo '<br>';
+		                	echo '</div>';
+                        echo '</div>';
+               
+						
 					}
 				 ?>
 			</div>
@@ -81,11 +117,19 @@ $replyResult = $conn->query($su);
 
 				</div>
 			</div>
+			 <?php
+              
+        	 if ($_SESSION["username"]) {
+                                echo "<h5>" . "Logged in as: " . $SessionUser . "</5> <style> h5 {float:right; padding-right: 2%; font-style: italic} </style>";
+                            } else {
+                                echo "<h5>" . "You are not yet signed in." . "</h5> <style> h5 {float:right; padding-right: 2%; font-style: italic} </style>";
+                            }
+            ?>
 		</div>
 
 		<?php
-	include ('footer.php');
-?>
+			include ('footer.php');
+		?>
     </div>
 
 </body>
