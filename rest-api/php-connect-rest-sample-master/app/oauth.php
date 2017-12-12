879c8bd720d029a5e90e25de8602d8ab01155fdf
Omar Farooqi <k1631089@kcl.ac.uk>
2017-12-12 18:22:34 +0000

    t

--- rest-api/php-connect-rest-sample-master/app/oauth.php
+++ rest-api/php-connect-rest-sample-master/app/oauth.php
@@ -17,7 +17,8 @@
               Azure Active Directory (AD) finishes the authentication flow.
  */
 
-require_once __DIR__ . '/../vendor/autoload.php';
+// require_once __DIR__ . '/../vendor/autoload.php';
+require_once("/app/rest-api/php-connect-rest-sample-master/vendor/autoload.php");
 
 use Microsoft\Graph\Connect\Constants;
 
